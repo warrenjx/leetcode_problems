cb922e43b55781bcadca4e6a318abb132b5cf2b2
warrenjx <78093918+warrenjx@users.noreply.github.com>
2023-05-24 15:44:53 -0700

    Update 0125.Valid_Palindrome.py
    made if slightly faster

--- Easy/0125.Valid_Palindrome.py
+++ Easy/0125.Valid_Palindrome.py
@@ -12,7 +12,7 @@ class Solution(object):
         s = "".join(re.findall("[a-zA-Z0-9]+", s)).lower()
         
         # check character against its opposite position in the string
-        for i in range(len(s)): 
+        for i in range(len(s) / 2): # save time by only looping through "half"
             if s[i] != s[len(s) - i - 1]: 
                 return False
         
