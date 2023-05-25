2411478c55bd4efae51bf02b11fa8fc2428276f1
warrenjx <78093918+warrenjx@users.noreply.github.com>
2023-05-25 11:32:44 -0700

    Create 0167.Two_Sum_II_Input_Array_Is_Sorted.py

---
+++ Medium/0167.Two_Sum_II_Input_Array_Is_Sorted.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right: 
+            sum = numbers[left] + numbers[right]
+            if sum == target: 
+                return [left + 1, right + 1]
+            
+            # deciding what pointer to move
+            if sum > target: 
+                right -= 1
+            else: 
+                left += 1
+        
+        return [1, 1]
