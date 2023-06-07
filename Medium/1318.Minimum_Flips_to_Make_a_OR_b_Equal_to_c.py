8f79c109d0ea3d629cbbd9c3198bc4d52b9ea961
warrenjx <78093918+warrenjx@users.noreply.github.com>
2023-06-07 09:52:14 -0700

    Create 1318.Minimum_Flips_to_Make_a_OR_b_Equal_to_c.py

---
+++ Medium/1318.Minimum_Flips_to_Make_a_OR_b_Equal_to_c.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minFlips(self, a, b, c):
+        """
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: int
+        """
+
+        sol = 0
+        ab = a | b
+
+        i = 1
+        while i <= max(ab, c): 
+            if (i & c) and not (i & b) and not (i & a): # c is 1 but a and b are both 0
+                sol += 1
+            elif not (i & c) and (i & a) and not (i & b): # c is 0 and a is 1
+                sol += 1
+            elif not (i & c) and (i & b) and not (i & a): # c is 0 and b is 1
+                sol += 1
+            elif not (i & c) and (i & b) and (i & b): # c is 0 and a and b are both  1: 
+                sol += 2
+
+            i = i << 1
+
+        return sol
