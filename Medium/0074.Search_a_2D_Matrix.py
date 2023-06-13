04082cfdd9922e0288936f35787c4e3e19c23034
warrenjx <78093918+warrenjx@users.noreply.github.com>
2023-06-13 10:04:40 -0700

    Update 0074.Search_a_2D_Matrix.py

--- Medium/0074.Search_a_2D_Matrix.py
+++ Medium/0074.Search_a_2D_Matrix.py
@@ -18,4 +18,42 @@ class Solution(object):
                 return True
 
         return False
-                 
+                
+# Binary Search SOLUTION: 
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+
+        # binary search rows to find correct row
+        l = 0
+        r = len(matrix) - 1
+
+        row = 0
+        while l <= r: 
+            mid = (l + r) // 2
+
+            if matrix[mid][0] > target: 
+                r = mid - 1
+            else: 
+                l = mid + 1
+        row = r
+        
+        # binary search the row to find correct number
+        l = 0
+        r = len(matrix[row]) - 1
+
+        while l <= r: 
+            mid = (l + r) // 2
+
+            if matrix[row][mid] == target: 
+                return True
+            elif matrix[row][mid] > target: 
+                r = mid - 1
+            else: 
+                l = mid + 1
+
+        return False
