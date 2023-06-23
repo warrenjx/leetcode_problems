a52f1cfd3a04778c691c767fc23cd846f96e3fe2
warrenjx <78093918+warrenjx@users.noreply.github.com>
2023-06-23 12:39:14 -0700

    Create 0070.Climbing_Stairs.py

---
+++ Easy/0070.Climbing_Stairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1: 
+            return 1
+        
+        dp = [1, 2]
+
+        for i in range(2, n): 
+            dp.append(dp[i - 1] + dp[i - 2])
+        
+        return dp[n - 1]
