892b2efff26f55f9a5ff7da9c36bb5785fb0d9f0
warrenjx <78093918+warrenjx@users.noreply.github.com>
2023-08-18 12:30:14 -0700

    Update 0435.Non-overlapping_Intervals.py

--- Medium/0435.Non-overlapping_Intervals.py
+++ Medium/0435.Non-overlapping_Intervals.py
@@ -4,22 +4,19 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        # edge case: only 1 interval
-        if len(intervals) == 1: 
-            return 0
-
-        # get intervals sorted by start
-        intervals.sort(key=lambda x:x[0])
+        # sorting intervals makes it significantly easier
+        intervals.sort(key = lambda x: x[0])
 
         sol = 0
 
-        for i in range(1, len(intervals)):
+        # start iterating at 1 because we compare to interval before
+        for i in range(1, len(intervals)): 
             # if there is an overlap
             if intervals[i][0] < intervals[i - 1][1]: 
+                # increment solution
                 sol += 1
 
-                # change later interval to smaller of the 2, simulates removing the larger one
-                if intervals[i][1] > intervals[i - 1][1]: 
-                    intervals[i][1] = intervals[i - 1][1]
+                # set end of current interval to min, simulating removing the LARGER interval
+                intervals[i][1] = min(intervals[i][1], intervals[i - 1][1])
 
         return sol
